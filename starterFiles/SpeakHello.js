e312a2c2b07ea4e29f3a483464a8b9f55dbca65e
immaryam <122366273+immaryam@users.noreply.github.com>
2024-10-24 01:32:58 +0300

    Update SpeakHello.js

--- starterFiles/SpeakHello.js
+++ starterFiles/SpeakHello.js
@@ -22,5 +22,5 @@ helloSpeaker.speak = function(name) {
 // 'helloSpeaker' on the global scope as well.
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
-// value.helloSpeaker = helloSpeaker;
+value.helloSpeaker = helloSpeaker;
 })(value);
